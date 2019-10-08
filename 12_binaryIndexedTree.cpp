ed3ca491e642908cf87a0a4e9f52ca04d160d5aa
Abdullahil Baki Adol <41238152+BakiAdol@users.noreply.github.com>
2019-10-09 01:39:24 +0600

    Update 12_binaryIndexedTree.cpp

--- 12_binaryIndexedTree.cpp
+++ 12_binaryIndexedTree.cpp
@@ -27,6 +27,22 @@ void update(int i, int value, int n)
     }
 }
 
+int spacificIndexValue(int indx)
+{
+    int sum=tree[indx];
+    if(indx>1)
+    {
+        int previous=indx-(indx & -indx);
+        indx--;
+        while(previous!=indx)
+        {
+            sum-=tree[indx];
+            indx-=(indx & -indx);
+        }
+    }
+    return sum;
+}
+
 int main()
 {
     int n=10,a;
@@ -38,8 +54,8 @@ int main()
     cout << "Sum of index 1 to 10 = " << query(10) << endl;
     cout << "Sum of index 1 to 5 = " << query(5) << endl;
     cout << "Update index 3 to value 10" << endl;
-    update(3,10,n);
+    update(3,10,n); /// add value 10 to index 3
     cout << "Sum of index 1 to 5 = " << query(5) << endl;
+    cout << "Index value of 6 is " << spacificIndexValue(6) << endl;
     return 0;
 }
-
